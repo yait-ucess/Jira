f0c5237e8a7c6027bd75de636064de595b3efc94
yait-ucess <bandai28@ezweb.ne.jp>
2021-06-20 14:51:44 +0900

    非同期関数作成

--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -3,6 +3,113 @@ import { RootState } from '../../app/store';
 import axios from 'axios';
 import { READ_TASK, POST_TASK, TASK_STATE, USER, CATEGORY } from '../types';
 
+export const fetchAsyncGetTasks = createAsyncThunk("task/getTask", async () => {
+  const res = await axios.get<READ_TASK[]>(
+    `${process.env.REACT_APP_API_URL}api/tasks/`,
+    {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    }
+  );
+  return res.data;
+});
+
+export const fetchAsyncGetUsers = createAsyncThunk(
+  "task/getUsers",
+  async () => {
+    const res = await axios.get<USER[]>(
+      `${process.env.REACT_APP_API_URL}api/users/`,
+      {
+        headers: {
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncGetCategory = createAsyncThunk(
+  "task/getCategory",
+  async () => {
+    const res = await axios.get<CATEGORY[]>(
+      `${process.env.REACT_APP_API_URL}api/category/`,
+      {
+        headers: {
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncCreateCategory = createAsyncThunk(
+  "task/createCategory",
+  async (item: string) => {
+    const res = await axios.post<CATEGORY>(
+      `${process.env.REACT_APP_API_URL}api/category/`,
+      { item: item },
+      {
+        headers: {
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncCreateTask = createAsyncThunk(
+  "task/createTask",
+  async (task: POST_TASK) => {
+    const res = await axios.post<READ_TASK>(
+      `${process.env.REACT_APP_API_URL}api/tasks/`,
+      task,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncUpdateTask = createAsyncThunk(
+  "task/updateTask",
+  async (task: POST_TASK) => {
+    const res = await axios.put<READ_TASK>(
+      `${process.env.REACT_APP_API_URL}api/tasks/${task.id}/`,
+      task,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncDeleteTask = createAsyncThunk(
+  "task/deleteTask",
+  async (id: number) => {
+    await axios.delete(`${process.env.REACT_APP_API_URL}api/tasks/${id}/`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return id;
+  }
+);
+
+
+
 export const initialState: TASK_STATE = {
   tasks: [
     {
