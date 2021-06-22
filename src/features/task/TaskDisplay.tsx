d043f4a3d8db81a0dbd7bcfe4f6d57dc0bce182b
yait-ucess <bandai28@ezweb.ne.jp>
2021-06-22 10:03:55 +0900

    Task Display Comonent 作成
    ・Projectを選択すると、詳細が右50%に表示される。

--- src/features/task/TaskDisplay.tsx
+++ src/features/task/TaskDisplay.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectSelectedTask } from './taskSlice';
+import { Table, TableBody, TableCell, TableRow } from '@material-ui/core';
 
 const TaskDisplay: React.FC = () => {
+  const selectedTask = useSelector(selectSelectedTask);
+  const rows = [
+    { item: "Task", data: selectedTask.task },
+    { item: "Description", data: selectedTask.description },
+    { item: "Criteria", data: selectedTask.criteria },
+    { item: "Owner", data: selectedTask.owner },
+    { item: "Responsible", data: selectedTask.responsible },
+    { item: "Estimate [days]", data: selectedTask.estimate },
+    { item: "Category", data: selectedTask.category_item },
+    { item: "Status", data: selectedTask.status_name },
+    { item: "Created", data: selectedTask.created_at },
+    { item: "Updated", data: selectedTask.updated_at },
+  ];
+
+  if (!selectedTask.task) {
+    return null;
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h2>Project deteils</h2>
+      <Table>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.item}>
+              <TableCell>
+                <strong>{row.item}</strong>
+              </TableCell>
+              <TableCell align="center">
+                {row.data}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
 
 export default TaskDisplay;
