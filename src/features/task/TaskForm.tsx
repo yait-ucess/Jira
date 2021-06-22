d5c745bb891f39f0900285bdabb97f11c335696e
yait-ucess <bandai28@ezweb.ne.jp>
2021-06-22 12:06:35 +0900

    Task Form Componentの、ModalやForm用の関数、state を作成。

--- src/features/task/TaskForm.tsx
+++ src/features/task/TaskForm.tsx
@@ -1,6 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  TextField,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Button,
+  Fab,
+  Modal
+} from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
+import AddIcon from '@material-ui/icons/Add';
+
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  fetchAsyncCreateTask,
+  fetchAsyncUpdateTask,
+  fetchAsyncCreateCategory,
+  selectUsers,
+  selectEditedTask,
+  selectCategory,
+  editTask,
+  selectTask,
+} from './taskSlice';
+import { AppDispatch } from '../../app/store';
+import { initialState } from './taskSlice';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  field: {
+    margin: theme.spacing(2),
+    minHeight: 240,
+  },
+  button: {
+    margin: theme.spacing(3),
+  },
+  addIcon: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(2),
+  },
+  saveModal: {
+    marginTop: theme.spacing(4),
+    marginLeft: theme.spacing(2),
+  },
+  paper: {
+    position: "absolute",
+    textAlign: "center",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const TaskForm: React.FC = () => {
+  const classes = useStyles();
+  const dispatch: AppDispatch = useDispatch();
+
+  const users = useSelector(selectUsers);
+  const category = useSelector(selectCategory);
+  const editedTask = useSelector(selectEditedTask);
+
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
+  const [inputText, setInputText] = useState("");
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const isDisabled = 
+    editedTask.task.length === 0 ||
+    editedTask.description.length === 0 ||
+    editedTask.criteria.length === 0;
+
+  const isCatDisabled = inputText.length === 0;
+
+  const hendleInputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value: string | number =e.target.value;
+    const name = e.target.name;
+    if (name === "estimate") {
+      value = Number(value);
+    }
+    dispatch(editTask({ ...editedTask, [name]: value }));
+  };
+
+  const handleSelectRespChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    const value = e.target.value as number;
+    dispatch(editTask({ ...editedTask, responsible: value }));
+  };
+
+  const handleSelectStatusChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    const value = e.target.value as string;
+    dispatch(editTask({ ...editedTask, status: value }));
+  };
+
+  const handleSelectCatChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    const value = e.target.value as number;
+    dispatch(editTask({ ...editedTask, category: value }));
+  };
+
+  let userOptions = users.map((user) => (
+    <MenuItem key={user.id} value={user.id}>
+      {user.username}
+    </MenuItem>
+  ));
+
+  let catOptions = category.map((cat) => (
+    <MenuItem key={cat.id} value={cat.id}>
+      {cat.item}
+    </MenuItem>
+  ));
+
   return (
     <div>
       
